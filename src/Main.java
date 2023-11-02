2b9f5f7bafdcd994e217d28d5c512bd8422bf8b4
denis <deniz246@mail.ru>
2023-11-02 14:20:34 +0300

    отправка согласно дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    String firstName = "Семён ";
+    String middleName = "Семёнович ";
+    String lastName = "Иванов ";
+    String fullName = lastName + firstName + middleName;
+    fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника " + fullName.toUpperCase());
     }
 }
